61e620301f7a619efc63c1ec33c1995cce333367
Grisha <veryrich2022@gmail.com>
2024-05-29 09:17:18 +0500

    data base

--- libs/DataBase.php
+++ libs/DataBase.php
@@ -4,5 +4,47 @@ namespace Dependency;
 
 class DataBase
 {
+    private $connect;
+    private $result;
 
+    public function __construct(string $host, string $dbName, string $login, string $password) 
+    {
+        $this->connect = pg_connect("host={$host} dbname={$dbName} user={$login} password={$password}");
+    }
+
+    public function checkConnect() 
+    {
+        $result = true;
+        if ($this->connect == false) {
+            $result = false;
+        }
+
+        return $result;
+    }
+
+    public function set(string $taleName, array $data): bool
+    {
+        $ret = true;
+        $query = "INSERT INTO {$tableName} (name, password, color) VALUES ($1, $2, $3)";
+        $result = pg_query_params($this->connection, $query, $data); 
+        if ($result == false) {
+            $ret = false;
+        }
+
+        return $ret;
+        
+    }
+    
+
+    public function get(string $tableName, array $id)
+    {
+        $query = "SELECT * FROM {$tableName} WHERE id = $1";
+        $result = pg_query_params($this->connection, $query, $id);
+        return $result;
+    }
+
+    public function delete(int $id) 
+    {
+
+    }
 }
--- test.php
+++ test.php
@@ -1,5 +1,26 @@
 <?php
-require 'libs/Validator.php';
 
-$valid = new \Dependency\Validator();
+$connect = pg_connect("host=localhost dbname=bot_user user=postgres password=root");
+//phpinfo();
 
+if ($connect == false) {
+    echo "Подключение не сработало\n";
+} else {
+    echo "Подключение сработало\n";
+}
+$array = array(3, 'Name');
+$query = "SELECT * FROM users";
+$result = pg_fetch_all(pg_query($connect, $query));
+// //var_dump($result);
+// //print_r($result);
+// while($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+//     foreach ($line as $value) {
+//         echo $value . "\n";
+//     }
+// }
+// while ($result) {
+    foreach ($result as $value) {
+        echo $value['id'] . "\n";
+        echo $value['name'] . "\n";
+    }
+//}
